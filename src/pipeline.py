4d9adf002ab6f9175098dc09df23362ed3a8bb97
Radia Belhocine <radia.belhocine@businessdecision.be>
2020-03-09 20:28:46 +0100

    Adding data to git

--- src/pipeline.py
+++ src/pipeline.py
@@ -8,6 +8,12 @@ load_data = PdfLoadData()
 text = load_data.process("./data/input_pdf/RVVB.A.0010.0025.pdf")
 text2 = load_data.process("./data/input_pdf/RVVB.A.1819.1356.pdf")
 
+import pandas as pd
+text_names = pd.read_csv("./data/arrest_names_new.csv")
+names_tags= pd.read_csv("./data/arrests_trefwoorden.csv", encoding='latin1')
+text_tags= pd.merge(text_names, names_tags, how="left",left_on='EVR_nr', right_on='Nr_Arrest')[['A_names', 'EVR_nr','Trefwoorden']]
+
+
 list_tags = ["TAG1", "TAG2", "TAG3"]
 preprocessing = Preprocessing([text, text2], list_tags)
 
