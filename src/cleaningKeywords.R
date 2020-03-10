fe2d2c9e2d234f539aa7a66b8e7211b0f148b1f4
Radia Belhocine <radia.belhocine@businessdecision.be>
2020-03-10 15:56:40 +0100

    Cleaning tags V2

--- src/cleaningKeywords.R
+++ src/cleaningKeywords.R
@@ -20,25 +20,51 @@ tags_new <- tags%>%
   select(new_tags, Nr_Arrest)%>%
   unique()%>%
   mutate(new_tags=tolower(new_tags))%>%
+  filter(!grepl("\\d", new_tags))%>%
+  mutate(new_tags= gsub("\\(navigator\\)|\\[navigator|navigator\\]|navigator", "", new_tags))%>%
+  filter(grepl("[A-Za-z]", new_tags))%>%
+  mutate(new_tags=gsub("[[:punct:]]", "", new_tags))%>%
+  mutate(new_tags=case_when(grepl("goede ro", new_tags)~"gro",
+                            grepl("exceptie verworpen", new_tags)~"Exceptie onontvankelijk", 
+                            grepl("schorsing verworpen", new_tags) ~ "Vernietiging verworpen",
+                            TRUE~new_tags))%>%
+  mutate(new_tags=trimws(new_tags))%>%
+  mutate(new_tags=tolower(new_tags))%>%
   filter(new_tags%in%hierarchy$Sub_tag | new_tags%in%hierarchy$Hoofd_tag)
 
 tags_old <- tags%>%
-  select(new_tags, Nr_Arrest)%>%
   unique()%>%
   mutate(new_tags=tolower(new_tags))%>%
-  filter(!(new_tags%in%hierarchy$Sub_tag | new_tags%in%hierarchy$Hoofd_tag))
-
-clean <- tags_old%>%
   filter(!grepl("\\d", new_tags))%>%
-  mutate(new_tags= gsub("\\(navigator\\)|\\[navigator|navigator\\]|navigator", "", new_tags))
-
-out <- tags_old%>%
-  filter(grepl("\\d", new_tags))
-
-
-
-length(unique(tags_new$Nr_Arrest))
-length(unique(tags$Nr_Arrest))
+  mutate(new_tags= gsub("\\(navigator\\)|\\[navigator|navigator\\]|navigator", "", new_tags))%>%
+  filter(grepl("[A-Za-z]", new_tags))%>%
+  mutate(new_tags=gsub("[[:punct:]]", "", new_tags))%>%
+  mutate(new_tags=case_when(grepl("goede ro", new_tags)~"gro",
+                            grepl("exceptie verworpen", new_tags)~"Exceptie onontvankelijk", 
+                            grepl("schorsing verworpen", new_tags) ~ "Vernietiging verworpen",
+                            TRUE~new_tags))%>%
+  mutate(new_tags=trimws(new_tags))%>%
+  mutate(new_tags=tolower(new_tags))%>%
+  filter(!(new_tags%in%hierarchy$Sub_tag | new_tags%in%hierarchy$Hoofd_tag))
+# 
+# clean <- tags_old%>%
+#   filter(!grepl("\\d", new_tags))%>%
+#   mutate(new_tags= gsub("\\(navigator\\)|\\[navigator|navigator\\]|navigator", "", new_tags))%>%
+#   filter(grepl("[A-Za-z]", new_tags))%>%
+#   mutate(new_tags=gsub("[[:punct:]]", "", new_tags))%>%
+#   mutate(new_tags=case_when(grepl("goede ro", new_tags)~"gro",
+#                             grepl("exceptie verworpen", new_tags)~"Exceptie onontvankelijk", 
+#                             grepl("schorsing verworpen", new_tags) ~ "Vernietiging verworpen",
+#                             TRUE~new_tags))%>%
+#   mutate(new_tags=trimws(new_tags))
+# 
+# out <- tags_old%>%
+#   filter(grepl("\\d", new_tags))
+# 
+# 
+# 
+# length(unique(tags_new$Nr_Arrest))
+# length(unique(tags$Nr_Arrest))
 
 tags_new_count <- tags_new%>%
   select(new_tags, Nr_Arrest)%>%
@@ -46,7 +72,7 @@ tags_new_count <- tags_new%>%
   group_by(new_tags)%>%
   summarise(ntext=n())
 
-tags_old_count <- clean%>%
+tags_old_count <- tags_old%>%
   select(new_tags, Nr_Arrest)%>%
   unique()%>%
   group_by(new_tags)%>%
